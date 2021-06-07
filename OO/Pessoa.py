2da3848c235864fc5d4d0bb9dce04f58da807bc3
Lucas Mattos <lcsmattos7@gmail.com>
2021-06-07 12:20:52 -0300

    Modificações: - Criação da classe Pessoa - Criado método cumprimentar

--- OO/Pessoa.py
+++ OO/Pessoa.py
@@ -1,3 +1,10 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        nome = str(input('Qual seu nome?: '))
+        return f'Olá {nome}'
 
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(type(p))
+    print(p.cumprimentar())
