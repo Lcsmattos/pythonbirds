30d34ccdd5a68102b6f36f35ceca6e3fd13e27d6
Lucas Mattos <lcsmattos7@gmail.com>
2021-06-14 16:23:51 -0300

    Arquivo de estudo e testes

--- OO/Pessoa.py
+++ OO/Pessoa.py
@@ -1,10 +1,28 @@
-class Pessoa:
-    def cumprimentar(self):
-        nome = str(input('Qual seu nome?: '))
-        return f'Olá {nome}'
+# class Pessoa:
+#     def cumprimentar(self):
+#         nome = str(input('Qual seu nome?: '))
+#         return f'Olá {nome}'
+#
+#
+# if __name__ == '__main__':
+#     p = Pessoa()
+#     print(type(p))
+#     print(p.cumprimentar())
 
 
-if __name__ == '__main__':
-    p = Pessoa()
-    print(type(p))
-    print(p.cumprimentar())
+# Testes
+
+class Humano:
+    def __init__(self, nome=None, idade=int, sexo=str):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+
+
+humano1 = Humano('Lucas', 28, 'Homem')
+humano2 = Humano('Ana', 28, 'Mulher')
+humano3 = Humano('Bob', 150, 'Android')
+
+print(humano1.nome)
+print(humano2)
+print(humano3)
